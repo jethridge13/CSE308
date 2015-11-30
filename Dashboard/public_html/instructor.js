fcf5a9a2e92ef42ab6c974cca65c80e15df26a0c
Faxiom <aaborger@gmail.com>
2015-11-30 16:46:31 -0500

    Basic email stuff added

--- Dashboard/public_html/instructor.js
+++ Dashboard/public_html/instructor.js
@@ -12,15 +12,30 @@ $(document).ready(function(){
         }
     });
     
+    var submitting = false;
+    
     $("#create_exam_submit").click(function(){
         // SUBMIT DETAILS TO SERVER
         var date_begin = $("#create_exam_date_begin_input").val();
         var date_end = $("#create_exam_date_end_input").val();
         var section = $("#create_exam_section_input").val();
         
+        submitting = true;
+        
+        $("#create_exam_submit").prop("disabled", true);
         $("#create_exam_submit").prop("class", "btn btn-default btn-warning");
         $("#create_exam_submit").html("<span id='create_exam_loading_icon' class='glyphicon glyphicon-refresh glyphicon-refresh-animate'></span> Submitting...");
+    });
+    
+    $("#create_exam_close").click(function(){
+        if (submitting) {
+            $("#create_exam_submit").prop("class", "btn btn-default");
+            $("#create_exam_submit").html("<span id='create_exam_loading_icon' class='glyphicon glyphicon-refresh'></span> Submit");
+            
+            submitting = false;
+        }
         
+        $("#create_exam_submit").prop("disabled", true);
         $("#create_exam_date_begin_input").val("");
         $("#create_exam_date_end_input").val("");
         $("#create_exam_section_input").val("");
