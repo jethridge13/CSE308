4bd84ba0bacc518a5386497212c0ab1c2890bb68
Faxiom <aaborger@gmail.com>
2015-11-29 20:28:35 -0500

    Added create exam submit button

--- Dashboard/public_html/instructor.js
+++ Dashboard/public_html/instructor.js
@@ -1,4 +1,21 @@
 $(document).ready(function(){
     $("#create_exam_date_begin").datepicker();
     $("#create_exam_date_end").datepicker();
+    
+    $("#create_exam_date_begin, #create_exam_date_end, #create_exam_section").change(function(){
+        if ($("#create_exam_date_begin_input").val().length > 0 &&
+            $("#create_exam_date_end_input").val().length > 0 &&
+            $("#create_exam_section_input").val().length > 0) {
+            $("#create_exam_submit").prop("disabled", false);
+        } else {
+            $("#create_exam_submit").prop("disabled", true);
+        }
+    });
+    
+    $("#create_exam_submit").click(function(){
+        // SUBMIT DETAILS TO SERVER
+        var date_begin = $("#create_exam_date_begin_input").val();
+        var date_end = $("#create_exam_date_end_input").val();
+        var section = $("#create_exam_section_input").val();
+    });
 });
