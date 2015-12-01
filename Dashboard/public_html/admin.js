311977cf148d37b93c48e4b5392cc0642c805112
Austin Borger <aaborger@gmail.com>
2015-12-01 17:32:18 -0500

    View Exam Dialog Added to all dashboards

--- Dashboard/public_html/admin.js
+++ Dashboard/public_html/admin.js
@@ -4,4 +4,20 @@ function respondToCalendar() {
 
 $(document).ready(function(){
     $("#calendar")[0].addEventListener("initialized", respondToCalendar);
+    
+    $("#calendar")[0].addEventListener("day_click", function(e){
+        var element = e.element;
+        
+        if (element.hasEvent) {
+            // CLEAR MODAL BODY OF PREVIOUS CONTENT
+            var view_exam_body = $("#view_exam_body");
+            
+            view_exam_body[0].innerHTML = "";
+            
+            // INITIALIZE MODAL BODY WITH EXAM INFO
+            
+            // SHOW MODAL
+            $("#view_exam").modal("show");
+        }
+    });
 });
